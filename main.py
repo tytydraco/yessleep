fa88b196a493387a4c396af362f66d06193d8d32
Tyler Nijmeh <tylernij@gmail.com>
2022-05-19 18:08:52 -0700

    Rebase
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ main.py
@@ -0,0 +1,36 @@
+import os.path
+
+import praw
+
+SUBREDDIT = 'nosleep'
+ID = os.getenv('REDDIT_ID')
+SECRET = os.getenv('REDDIT_SECRET')
+OUTPUT_DIR = 'out/'
+
+if not os.path.exists(OUTPUT_DIR):
+    os.mkdir(OUTPUT_DIR)
+
+# Create PRAW instance
+reddit = praw.Reddit(
+    client_id=ID,
+    client_secret=SECRET,
+    user_agent='scraper'
+)
+
+# Fetch top posts
+subreddit = reddit.subreddit(SUBREDDIT)
+hot_posts = subreddit.hot(limit=None)
+
+# Go through all hot posts
+for post in hot_posts:
+    if post.stickied or not post.is_self:
+        continue
+
+    print(post.title)
+
+    sanitized_title = post.title.replace(' ', '_')
+    sanitized_title = ''.join(ch for ch in sanitized_title if (ch.isalnum() or ch == '_'))
+    sanitized_content = post.selftext.encode('utf8')
+
+    with open(OUTPUT_DIR + sanitized_title + '.md', 'wb') as f:
+        f.write(sanitized_content)
